43075dd682bfcca39eb7a3580d81468438bd5a47
Vasilev Sergei <200895v@mail.ru>
2026-05-03 13:16:59 +0300

    Initial Commit

---
+++ PhotoSorting/PhotoSortingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSortingApp.swift
+//  PhotoSorting
+//
+//  Created by Сергей Васильев on 03.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSortingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
